41f503018e7a6692cdf2cc558cc8bc413785c4cd
lucashudson-eng <lucashudson.eng@gmail.com>
2026-03-05 12:28:21 -0300

    hw-519 and esp32-s3-mini

--- libraries.py
+++ libraries.py
@@ -1,23 +1,28 @@
 import os
 import shutil
 
-root = "C:\\Users\\Lucas Hudson\\Desktop\\GitHub\\altium-lib"
+root = os.path.dirname(os.path.abspath(__file__))
 folder = "Project Outputs for "
 extension = ".IntLib"
-dest = 'C:\\Users\\Lucas Hudson\\Desktop\\GitHub\\altium-lib\\libraries'
+dest = os.path.join(root, 'libraries')
+
+if not os.path.exists(dest):
+    os.makedirs(dest)
 
 itens = os.listdir(root)
 dirs = [item for item in itens if os.path.isdir(os.path.join(root, item))]
-dirs.remove('.git')
-dirs.remove('libraries')
-dirs.remove('3D Parts')
+
+ignore_dirs = ['.git', 'libraries', '3D Parts']
+for ignore in ignore_dirs:
+    if ignore in dirs:
+        dirs.remove(ignore)
 
 for dir in dirs:
-    path = f'{root}\\{dir}\\{folder}{dir}'
+    path = os.path.join(root, dir, f'{folder}{dir}')
     if os.path.exists(path) and os.path.isdir(path):
-        ori = f'{path}\\{dir}{extension}'
+        ori = os.path.join(path, f'{dir}{extension}')
         if os.path.exists(ori) and os.path.isfile(ori):
-            file_dest = f'{dest}\\{dir}{extension}'
+            file_dest = os.path.join(dest, f'{dir}{extension}')
             if os.path.exists(file_dest):
                 os.remove(file_dest)
             shutil.move(ori, dest)
